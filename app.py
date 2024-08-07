fe80fb069c6aeabdfdb6579d9eb885fcf8d17ae1
nazaralievas <sadanaza4@gmail.com>
2024-08-07 16:00:55 +0600

    --

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+# создаём базу данных
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/', methods=["GET", "POST"])
 def homepage():
-    return render_template('homepage.html')
+    tasks = Todo.query.all()
+    if request.method == "POST":
+        pass
+    return render_template('homepage.html', tasks=tasks)
 
 
 @app.route('/about')
