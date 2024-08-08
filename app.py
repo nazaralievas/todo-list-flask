54c78e350b10ca2afebab6d6c74ef34f14b589e2
nazaralievas <sadanaza4@gmail.com>
2024-08-08 11:01:33 +0600

    --

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -21,8 +21,26 @@ with app.app_context():
 def homepage():
     tasks = Todo.query.all()
     if request.method == "POST":
-        pass
+        text = request.form['text']
+        task = Todo(text=text)
+        db.session.add(task)
+        db.session.commit()
+        return redirect('/')
+
     return render_template('homepage.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task = Todo.query.get(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect('/')
+
+
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    task = Todo.query.get(id)
+    return render_template('update.html', task=task)
 
 
 @app.route('/about')
